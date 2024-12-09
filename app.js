27ae7adde7f3d0e153004e9b6937e7d57d4446cb
Zachary Springer <zdspringer99@gmail.com>
2024-12-08 17:18:39 -0800

    Added app.js to run the program

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mariadb = require("mariadb");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+//Default Route
+app.get("/", (req, res) => {
+    console.log("Hello World! Server!");
+
+    res.render("home");
+})
+
+//Connect to Server
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+})
